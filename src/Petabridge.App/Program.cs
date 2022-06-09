8868279aa35d8d7e5fc5fff67fba8c33b1cdf1a8
Aaron Stannard <aaron@petabridge.com>
2022-06-09 09:46:27 -0500

    converted to Akka.Hosting, .NET 6 file-scoped namespaces (#133)

--- src/Petabridge.App/Program.cs
+++ src/Petabridge.App/Program.cs
@@ -1,31 +1,64 @@
-﻿using System.Threading.Tasks;
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Cluster.Hosting;
+using Akka.Hosting;
+using Akka.Remote.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Petabridge.Cmd.Cluster;
+using Petabridge.Cmd.Cluster.Sharding;
+using Petabridge.Cmd.Host;
+using Petabridge.Cmd.Remote;
 
-namespace Petabridge.App
+namespace Petabridge.App;
+
+public class Program
 {
-    public class Program
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
-        {
-            var host = new HostBuilder()
-                .ConfigureServices((hostContext, services) =>
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+        
+        var host = new HostBuilder()
+            .ConfigureHostConfiguration(builder => 
+                builder.AddEnvironmentVariables()
+                    .AddJsonFile("appsettings.json")
+                    .AddJsonFile($"appsettings.{environment}.json"))
+            .ConfigureServices((hostContext, services) =>
+            {
+                var akkaConfig = hostContext.Configuration.GetRequiredSection(nameof(AkkaClusterConfig))
+                    .Get<AkkaClusterConfig>();
+                services.AddLogging();
+                services.AddAkka(akkaConfig.ActorSystemName, (builder, provider) =>
                 {
-                    services.AddLogging();
-                    services.AddHostedService<AkkaService>(); // runs Akka.NET
- 
-                })
-                .ConfigureLogging((hostContext, configLogging) =>
-                {
-                    configLogging.AddConsole();
- 
-                })
-                .UseConsoleLifetime()
-                .Build();
+                    Debug.Assert(akkaConfig.Port != null, "akkaConfig.Port != null");
+                    builder.AddHocon(File.ReadAllText("app.conf"))
+                        .WithRemoting(akkaConfig.Hostname, akkaConfig.Port.Value)
+                        .WithClustering(new ClusterOptions()
+                        {
+                            Roles = akkaConfig.Roles?.ToArray() ?? Array.Empty<string>(),
+                            SeedNodes = akkaConfig.SeedNodes?.Select(Address.Parse).ToArray() ?? Array.Empty<Address>()
+                        })
+                        .AddPetabridgeCmd(cmd =>
+                        {
+                            cmd.RegisterCommandPalette(new RemoteCommands());
+                            cmd.RegisterCommandPalette(ClusterCommands.Instance);
+                            
+                            // sharding commands, although the app isn't configured to host any by default
+                            cmd.RegisterCommandPalette(ClusterShardingCommands.Instance);
+                        });
+                });
 
-            await host.RunAsync();
-        }
+            })
+            .ConfigureLogging((hostContext, configLogging) =>
+            {
+                configLogging.AddConsole();
+            })
+            .UseConsoleLifetime()
+            .Build();
+
+        await host.RunAsync();
     }
-   
 }
