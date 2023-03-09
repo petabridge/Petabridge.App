bd3517180264a7aee40a1c93c77a774824668a5b
Ebere Abanonu <eaba@users.noreply.github.com>
2023-03-09 14:43:22 +0100

    [add] Blame Crash

--- build/Build.cs
+++ build/Build.cs
@@ -320,6 +320,7 @@ partial class Build : NukeBuild
                            .SetResultsDirectory(OutputTests)
                            .SetProcessWorkingDirectory(Directory.GetParent(project).FullName)
                            .SetLoggers("trx")
+                           .SetBlameCrash(true)
                            .SetBlameHang(true)
                            .SetBlameHangTimeout(TestTimeout)
                            .SetVerbosity(verbosity: DotNetVerbosity.Normal)
