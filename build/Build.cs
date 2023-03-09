65a7ec36be55cb9722dc51d15900d02f6b7daa55
Ebere Abanonu <eaba@users.noreply.github.com>
2023-03-09 16:32:55 +0100

    increase timeout to 30m

--- build/Build.cs
+++ build/Build.cs
@@ -60,7 +60,7 @@ partial class Build : NukeBuild
 
     //usage:
     //./build.cmd runtests --test-timeout 300s
-    [Parameter] string TestTimeout = "5m";
+    [Parameter] string TestTimeout = "30m";
 
     [Parameter] [Secret] string DockerUsername;
     [Parameter] [Secret] string DockerPassword;
